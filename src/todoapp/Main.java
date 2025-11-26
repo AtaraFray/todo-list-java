13d692a6166fb8dec856cd1d01472adc00710c55
AtaraFray <al7139697@gmail.com>
2025-11-26 15:50:43 +0200

    debug addNewTask method that add new task to JSON file in TaskRepository class

--- src/todoapp/Main.java
+++ src/todoapp/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// בדיקת פונקציות curd
+		TaskRepository taskes = new TaskRepository();
+		Task task = new Task(5, "finish task", "finisn java task", Status.IN_PROGRESS);
+		taskes.addNewTask(task);
 	}
 
 }
