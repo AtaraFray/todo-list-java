3686558a4ad03f870fbaac34225a0a12045d0017
AtaraFray <al7139697@gmail.com>
2025-11-26 22:59:13 +0200

    Init UI with show all task and add new task and fix debug

--- src/todoapp/Main.java
+++ src/todoapp/Main.java
@@ -2,18 +2,53 @@ package todoapp;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TaskRepository repo = new TaskRepository();
-		TaskService service = new TaskService(repo);
-		Map<Integer, Task> t = TaskRepository.loadTasksFromFile();
-         List<Task> sort = service.sortTaskByStatus();
-		for (Task task : sort) {
-			task.toString();
+	public static void ShowAllTasks(TaskService service) {
+		for (Task task : service.sortTaskByStatus()) {
+			System.out.println(task.toString());
 		}
+	}
+
+	public static void addNewTask(Task newTask, TaskRepository taskRepository) {
+		taskRepository.addNewTask(newTask);
+	}
+
+	public static void main(String[] args) {
+
+		// ------------------------------------
+		// Basic UI
+		// ------------------------------------
+		System.out.println("                  ******************");
+		System.out.println("                  My Tasks Management");
+		System.out.println("                  ******************");
+
+		// Show all tasks
+		TaskRepository taskRepository = new TaskRepository();
+		TaskService service = new TaskService(taskRepository);
+		ShowAllTasks(service);
+
+		// Add new task form
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("---------------------------------------");
+		System.out.println("              Add task form             ");
+		System.out.println("enter taskId");
+		int taskId = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("enter task title");
+		String title = scanner.next();
+		System.out.println("enter description ");
+		scanner.nextLine();
+		String description = scanner.nextLine();
+		
+		System.out.println("---------------------------------------");
+		Task newTask = new Task(taskId, title, description);
+		addNewTask(newTask, taskRepository);
+		
+		
+		;
 	}
 
 }
---
+++ src/todoapp/Status.java
@@ -0,0 +1,5 @@
+package todoapp;
+
+public enum Status {
+			NEW, IN_PROGRESS, DONE
+}
