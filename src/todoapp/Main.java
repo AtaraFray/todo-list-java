2b9af0025b0a1a1b5e14c53852fe9b7331cbd9ac
AtaraFray <al7139697@gmail.com>
2025-11-26 16:54:04 +0200

    Debug succseful

--- src/todoapp/Main.java
+++ src/todoapp/Main.java
@@ -5,14 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// בדיקת פונקציות curd
-		TaskRepository taskes = new TaskRepository();
-		Task task = new Task(5, "finish task", "finisn java task", Status.DONE);
-		taskes.addNewTask(task);
-		System.out.println(taskes.updateTask(5, task));
-//		taskes.delete(5);
-		taskes.saveTasksToFile();
-		
 	}
 
 }
